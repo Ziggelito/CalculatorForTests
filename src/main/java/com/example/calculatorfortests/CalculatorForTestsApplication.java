617cf1067b47984fc2369b7958c7ebb105e0771b
Ziggelito <Sixten.Lonnberg@yh.nackademin.se>
2023-09-29 14:43:51 +0200

    Initial commit

---
+++ src/main/java/com/example/calculatorfortests/CalculatorForTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatorfortests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorForTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorForTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculatorfortests/CalculatorForTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatorfortests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorForTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
